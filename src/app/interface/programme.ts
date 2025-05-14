50b7263b97ed6fb841d3b7ae871fc5cbfb8762e5
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-14 09:30:50 +0100

    feat(interface): Implement programme interface.

---
+++ src/app/interface/programme.ts
@@ -0,0 +1,45 @@
+export interface Programme {
+
+    course: string,
+
+    educationLevel: string,
+
+    institution: string,
+
+    country: string,
+
+    discipline: string,
+
+    studyLanguage: string,
+
+    attendance: string,
+
+    description: string,
+
+    view: number,
+
+    duration: number,
+
+    level: string,
+
+    tuitionFee: string,
+
+    courseDetails: string,
+
+    structure: string,
+
+    fees: string,
+
+    housingFee: number,
+
+    acceptanceFee: number,
+
+    applicationFee: number,
+
+    livingFee: number,
+
+    campuses: string,
+
+    isOfficialPartner: boolean
+
+}
