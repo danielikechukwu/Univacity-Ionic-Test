2dce8e616c5853eebaab80deac1f93e6d8f220c1
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-15 22:26:24 +0100

    feat(modal): Implement a modal component.
    refact(search screen): implement select option and filter searchs.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption, IonItem, ModalController } from '@ionic/angular/standalone';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSelect, IonSelectOption, IonItem],
 })
 
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit, OnDestroy {
+
+  message = 'This modal example uses the modalController to present and dismiss modals.';
+
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnDestroy(): void {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  async openModal() {
+
+    const modal = await this.modalCtrl.create({
+      component: ModalComponent
+    });
+
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+
+    if(role === 'confirm'){
+      this.message = `Hello, ${data}!`;
+    }
+  }
+
+
+
+
 }
